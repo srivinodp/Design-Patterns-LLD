605ac05d5fb5e09b6a1a607baacd196741b8abc9
srivinodp <67648210+srivinodp@users.noreply.github.com>
2023-02-15 23:13:44 +0530

    Update ObserverPattern.py
    changed the pattern file according to the java changes.

--- ObserverPattern.py
+++ ObserverPattern.py
@@ -1,10 +1,61 @@
-class Observer:
-    def update(self, value):
+import random
+from abc import ABC, abstractmethod
+
+
+#Note - 
+# This Python code defines the Observer and Observable interfaces as abstract classes using the abc module. The Item class and ShoppingCart class are defined in a similar way to the Java implementation.
+
+# The main differences in the Python code are that we use the @abstractmethod decorator to define abstract methods in the Observer and Observable classes, and we use the __str__() method instead of toString() to print the Item object
+class Observer(ABC):
+    @abstractmethod
+    def update(self, observable):
         pass
 
-class Observable:
+class Observable(ABC):
+    @abstractmethod
+    def add_observer(self, observer):
+        pass
+
+    @abstractmethod
+    def remove_observer(self, observer):
+        pass
+
+    @abstractmethod
+    def notify_observers(self):
+        pass
+
+    @abstractmethod
+    def print_items(self):
+        pass
+
+class Item:
     def __init__(self):
+        self.id = random.randint(0, int(1e5))
+
+    def get_price(self):
+        return 0
+
+    def __str__(self):
+        return str(self.id)
+
+class ShoppingCart(Observable):
+    def __init__(self):
+        self.items = []
         self.observers = []
+
+    def add_item(self, item):
+        self.items.append(item)
+        self.notify_observers()
+
+    def remove_item(self, item):
+        self.items.remove(item)
+        self.notify_observers()
+
+    def get_total_price(self):
+        total_price = 0.0
+        for item in self.items:
+            total_price += item.get_price()
+        return total_price
 
     def add_observer(self, observer):
         self.observers.append(observer)
@@ -12,34 +63,17 @@ class Observable:
     def remove_observer(self, observer):
         self.observers.remove(observer)
 
-    def notify_observers(self, value):
+    def notify_observers(self):
         for observer in self.observers:
-            observer.update(value)
+            observer.update(self)
+        self.print_items()
+        print("--------------")
 
-class Data(Observable):
-    def __init__(self):
-        super().__init__()
-        self._value = 0
+    def print_items(self):
+        for item in self.items:
+            print(item)
 
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, value):
-        self._value = value
-        self.notify_observers(value)
-
-class DataObserver(Observer):
-    def update(self, value):
-        print(f"Value updated: {value}")
-
-data = Data()
-observer = DataObserver()
-data.add_observer(observer)
-data.value = 10
-data.value = 20
-#------Output
-#Value updated: 10
-#Value updated: 20
-
+if __name__ == '__main__':
+    s_cart = ShoppingCart()
+    for i in range(5):
+        s_cart.add_item(Item())
